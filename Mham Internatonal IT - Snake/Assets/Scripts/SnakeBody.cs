616ef73e32bf9741699bb0ed3b9d53406f01bc01
wedad <87364144+weex1997@users.noreply.github.com>
2022-06-13 18:07:23 +0300

    prototype

---
+++ Mham Internatonal IT - Snake/Assets/Scripts/SnakeBody.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeBody : MonoBehaviour
+{
+
+    private int myOrder;
+    public Transform head;
+
+    void Start()
+    {
+        head = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
+        for (int i = 0; i < head.GetComponent<SnakeMovement>().bodyParts.Count; i++)
+        {
+            if (gameObject == head.GetComponent<SnakeMovement>().bodyParts[i].gameObject)
+            {
+                myOrder = i;
+            }
+        }
+    }
+    private Vector3 movementVelocity;
+    [Range(0.0f, 1.0f)]
+    public float overTime = 0.5f;
+    void FixedUpdate()
+    {
+        if (myOrder == 0)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, head.position, ref movementVelocity, overTime);
+            transform.LookAt(head.transform.position);
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, head.GetComponent<SnakeMovement>().bodyParts[myOrder - 1].position, ref movementVelocity, overTime);
+            transform.LookAt(head.transform.position);
+        }
+    }
+}
---
+++ Mham Internatonal IT - Snake/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SnakeMovement : MonoBehaviour
+{
+    public List<Transform> bodyParts = new List<Transform>();
+
+    void Update()
+    {
+        MouseRotationSnake();
+   
+    }
+
+    private Vector3 pointInworld;
+    private Vector3 mousePosition;
+    private float radius = 3.0f;
+    private Vector3 direction;
+    void MouseRotationSnake()
+    {
+        Ray ray = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 1000.0f);
+        mousePosition = new Vector3(hit.point.x, hit.point.y, 0);
+        direction = mousePosition - transform.position;
+        direction.z = 0;
+        pointInworld = direction.normalized * radius + transform.position;
+        transform.LookAt(pointInworld);
+    }
+
+    public float speed = 3.5f;
+    public float currentRotaticn;
+    public float rotationSensitvity = 50.0f;
+    void FixedUpdate()
+    {
+
+        MoveForward();
+        CameraFollow();
+    }
+    void MoveForward()
+    {
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+
+    [Range(0.0f, 1.0f)]
+    public float smoothTime = 0.5f;
+    void CameraFollow()
+    {
+
+        Transform camera = GameObject.FindGameObjectWithTag("MainCamera").gameObject.transform;
+        Vector3 cameraVelocity = Vector3.zero;
+        camera.position = Vector3.SmoothDamp(camera.position,
+        new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10), ref cameraVelocity, smoothTime);
+    }
+
+
+    public Transform bodyObject;
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Orb")
+        {
+            Destroy(other.gameObject);
+            if (bodyParts.Count == 0)
+            {
+                Vector3 currentPos = transform.position;
+                Transform newBodyPart = Instantiate(bodyObject, currentPos, Quaternion.identity) as Transform;
+                bodyParts.Add(newBodyPart);
+            }
+            else
+            {
+                    Vector3 currentPos = bodyParts[bodyParts.Count-1].position;
+                    Transform newBodyPart = Instantiate(bodyObject, currentPos, Quaternion.identity) as Transform;
+                    bodyParts.Add(newBodyPart);
+                
+            }
+
+        }
+    }
+}
